e8abf6ea91cd23b8ed397f012737e3cc0d1f336a
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-01-30 16:03:19 -0500

    perf: :zap: Excercise 1 do it

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
+from excercises.exercise1 import exercise1
+
 while True:
     try:
         print(
-                    """
+            """
                 Bienvenido al sistema de Ejercicios del SENA :):)
                 Selecciona:
                 1.Ejercico 1
                 2.Ejercico 2
                 3.Ejercico 3
                 4.Ejercico 4
+                5.Ejercico 5
+                6.Ejercico 6
+                7.Ejercico 7
+                8.Ejercico 8
+                9.Ejercico 9
+                10.Ejercico 10
                 0.Salir
                 """
-                )
+        )
         opc = int(input())
+        if opc == 0:
+                break 
         try:
             while True:
                 match opc:
-                    case 1 : print("EXERCISES")
+                    case 1:
+                        exercise1()
+                        continuar = input("¿Desea calcular el tiempo para otro corredor? (s/n): ")
+                        if continuar.lower() != 's':
+                            break 
         except ValueError:
             print("Ingresa un numero porfa.")
     except ValueError:
