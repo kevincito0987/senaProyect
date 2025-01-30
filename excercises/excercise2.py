9cf15ef679afeed0f86e2f9fb546ec89655f76fd
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-01-30 16:09:35 -0500

    perf: :zap: Excercise 2 added

---
+++ excercises/excercise2.py
@@ -0,0 +1,12 @@
+def excercise2():
+    print(
+            """
+                Realizar la conversión de una temperatura dada en grados Centígrados a grados Fahrenheit (Fórmula: F =
+                (9/5) C + 32). 
+            """
+                )
+    
+    centigrados = float(input("Ingresa la temperatura en grados Centígrados: "))
+    fahrenheit = (centigrados * 9/5) + 32
+    print(f"La temperatura en grados Fahrenheit es {fahrenheit}")
+    
