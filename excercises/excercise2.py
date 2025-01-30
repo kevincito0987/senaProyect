91863b48454c650f04c17e2a20aa4671d308d10f
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-01-30 16:09:35 -0500

    perf: :zap: Excercise 2 added

--- excercises/excercise2.py
+++ excercises/excercise2.py
@@ -1,15 +1,12 @@
 def excercise2():
-    while True:
-                try:
-                    C = int(input("Ingrese la cantidad de grados celcios a convertir a farenheit, (0 para salir): "))
-                    if C < 0:
-                        raise ValueError("La cantidad de horas debe ser positiva.")
-                    elif C == 0:
-                     break
-                except ValueError as e:
-                    print(f"Error: {e}")
-
-    F = ((9/5) * C) + 32
-
-
-    print(f"La temperatura en grados Farenheit es de {F}")
+    print(
+            """
+                Realizar la conversión de una temperatura dada en grados Centígrados a grados Fahrenheit (Fórmula: F =
+                (9/5) C + 32). 
+            """
+                )
+    
+    centigrados = float(input("Ingresa la temperatura en grados Centígrados: "))
+    fahrenheit = (centigrados * 9/5) + 32
+    print(f"La temperatura en grados Fahrenheit es {fahrenheit}")
+    
